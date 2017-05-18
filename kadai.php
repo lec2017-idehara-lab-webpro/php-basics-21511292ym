c4dd7be975822f43129c1207b51f09fc2582c92a
21511292ym <21511292ym@tama.ac.jp>
2017-05-18 14:29:37 +0900

    php0518

--- kadai.php
+++ kadai.php
@@ -7,21 +7,67 @@
   <body>
 
 <?php
-  print('PHP の課題'); // この行を <h1> で表示しなさい。
+  print('<h1>PHP の課題</h1>'); // この行を <h1> で表示しなさい。
 
-  print('計算'); // この行を <h2> で表示しなさい。
+  print('<h2>計算</h2>'); // この行を <h2> で表示しなさい。
   $x = 10;
   $y = 20;
+      $wa = $x+$y;
+      $sa = $x-$y;
+      $seki = $x*$y;
+      $syou = $x/$y;
+      print("<table border=1>
+  <tr>
+　　<td>和</td>
+　　<td>$wa</td>
+  </tr>
+  <tr>
+　　<td>差</td>
+　　<td>$sa</td>
+  </tr>
+  <tr>
+　　<td>積</td>
+　　<td>$seki</td>
+  </tr>
+  <tr>
+　　<td>商</td>
+　　<td>$syou</td>
+  </tr>
+  </table>");
+     
 // $x と $yの和、差、積、商を php で計算して表示しなさい。
 // 可能であれば、表形式(table) で表示しなさい。
 
-  print('文字列'); // この行を <h2> で表示しなさい。
+  print('<h2>文字列</h2>'); // この行を <h2> で表示しなさい。
   $base = '21611';
   $personal = '499';
+      echo "$base$personal";
 // $base と $personal を連結し、表示しなさい。
 
-  print('変数定義のチェック'); // この行を <h2> で表示しなさい。
-
+  print('<h2>変数定義のチェック</h2>'); // この行を <h2> で表示しなさい。
+      
+        if (isset($x))
+        {
+   print("x 1 <br>");
+}
+      else{
+   print("x 1");
+}
+        if (isset($y))
+        {
+   print("y 1 <br>");
+}
+      else{
+   print('y 0');
+}
+    
+        if (isset($z))
+        {
+   print('z 1');
+}
+      else{
+   print('z 0');
+}
   // $x, $y, $z のそれぞれについて、その変数がこの場所で定義されているかどうか表示しなさい。
   // 動的にチェックすること。
  ?>
